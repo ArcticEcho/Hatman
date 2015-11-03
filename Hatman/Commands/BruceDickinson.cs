d9f4db2bb1c8ffd3d44b15ba104a8cd4f310593d
Mark Becwar <becwarm1@owls.southernct.edu>
2015-11-03 18:55:52 -0500

    Hatman needs more cowbell.

---
+++ Hatman/Commands/BruceDickinson.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using ChatExchangeDotNet;
+
+namespace Hatman.Commands
+{
+    public class Cowbell : ICommand
+    {
+        private static readonly Regex pattern = new Regex("(?i:needs something)|(?i:needs more)", Extensions.RegOpts);
+
+        public System.Text.RegularExpressions.Regex CommandPattern
+        {
+            get { return pattern; }
+        }
+
+        public string Description
+        {
+            get { return "Needs more cowbell"; }
+        }
+
+        public string Usage
+        {
+            get { return "[...]needs something[...]|[...]needs more[...]"; }
+        }
+
+        public void ProcessMessage(ChatExchangeDotNet.Message msg, ref ChatExchangeDotNet.Room rm)
+        {
+            rm.PostReplyFast(msg, "http://i.stack.imgur.com/3U9DQ.gif");
+        }
+    }
+
+    public class Fever : ICommand
+    {
+        private static readonly Regex pattern = new Regex("(?i:fever)", Extensions.RegOpts);
+
+        public System.Text.RegularExpressions.Regex CommandPattern
+        {
+            get { return pattern; }
+        }
+
+        public string Description
+        {
+            get { return "Gets advice from record producer Bruce Dickinson."; }
+        }
+
+        public string Usage
+        {
+            get { return "[...]fever[...]"; }
+        }
+
+        public void ProcessMessage(ChatExchangeDotNet.Message msg, ref ChatExchangeDotNet.Room rm)
+        {
+            rm.PostReplyFast(msg, "http://i.stack.imgur.com/edt7R.jpg");
+        }
+    }
+
+}
