4442eacebdd696f9eedca59c762551814b3e6fb5
Siguza <siguza@siguza.net>
2015-11-06 13:33:10 +0100

    Group "what is love" commands in one file

--- Hatman/Commands/WhatIsLove.cs
+++ Hatman/Commands/WhatIsLove.cs
@@ -36,4 +36,38 @@ namespace Hatman.Commands
             rm.PostReplyFast(msg, "Baby don't hurt me.");
         }
     }
+
+    class DontHurtMe : ICommand
+    {
+        private readonly Regex ptn = new Regex(@"(?i)^don'?t hurt me\.?$", Extensions.RegOpts);
+
+        public Regex CommandPattern
+        {
+            get
+            {
+                return ptn;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Sing along!";
+            }
+        }
+
+        public string Usage
+        {
+            get
+            {
+                return "Don't hurt me[.]";
+            }
+        }
+
+        public void ProcessMessage(Message msg, ref Room rm)
+        {
+            rm.PostReplyFast(msg, "No more.");
+        }
+    }
 }
