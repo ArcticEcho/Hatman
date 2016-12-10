307e4618c959410d248fdacc3ded583be79ce69f
Андрей Беньковский <abenkovskii@gmail.com>
2016-12-10 10:41:19 +0400

    Implemented rolling multiple dice at once e.g 3d6

--- Hatman/Commands/Dice.cs
+++ Hatman/Commands/Dice.cs
@@ -6,7 +6,7 @@ namespace Hatman.Commands
 {
     class Dice : ICommand
     {
-        private readonly Regex ptn = new Regex(@"(?i)^d(?![io])", Extensions.RegOpts);
+        private readonly Regex ptn = new Regex(@"^(\d*)[dD](\d+)$", Extensions.RegOpts);
         private readonly string[] errorPhrases = new[]
         {
             "https://i.imgflip.com/ucj8n.jpg",
@@ -21,39 +21,58 @@ namespace Hatman.Commands
 
         public string Description => "Throws a die.";
 
-        public string Usage => "d<number>";
+        public string Usage => "<optional number>d<number>";
 
 
 
         public void ProcessMessage(Message msg, ref Room rm)
         {
-            var up = msg.Content.Remove(0, 1);
-            ulong upInt = 0;
-
-            if (!ulong.TryParse(up, out upInt) || upInt == 0)
+            var match = ptn.Match(msg.Content);
+            if(!match.Success)
+            {
+                /* technically shold be dead code becuase we know the expression matched */
+                rm.PostReplyLight(msg, string.Format(errorPhrases.PickRandom(), msg.Author.Name));
+                return;
+            }
+            
+            string diceCountStr = match.Groups[1].Value;
+            ulong diceCount = 0;
+            if (diceCountStr == "")
+            {
+                diceCount = 1;
+            }
+            else if (!ulong.TryParse(diceCountStr, out diceCount) || diceCount == 0)
             {
                 rm.PostReplyLight(msg, string.Format(errorPhrases.PickRandom(), msg.Author.Name));
                 return;
             }
-
-            var nBytes = new byte[8];
-            Extensions.RNG.GetBytes(nBytes);
-            var n = (BitConverter.ToUInt64(nBytes, 0) % upInt) + 1;
-
-            if (n % 25 == 0)
+            
+            string edgeCountStr = match.Groups[2].Value;
+            ulong edgeCount = 0;
+            if (!ulong.TryParse(edgeCountStr, out edgeCount) || edgeCount == 0)
             {
-                rm.PostReplyLight(msg, "http://imgs.xkcd.com/comics/random_number.png");
+                rm.PostReplyLight(msg, string.Format(errorPhrases.PickRandom(), msg.Author.Name));
+                return;
+            }
+            
+            /* Protection against too large values. */
+            /* Do we need it? */
+            if (diceCount > 1000)
+            {
+                rm.PostReplyLight(msg, "I don't have that many dices");
                 return;
             }
 
-            if (upInt == 100)
+            var nBytes = new byte[8 * diceCount];
+            Extensions.RNG.GetBytes(nBytes);
+
+            var sum = 0;
+            for (uint i = 0; i < diceCount; ++i)
             {
-                rm.PostReplyLight(msg, n == 1 ? "**CRITICAL SUCCESS**" : n == 100 ? "**CRITICAL FAILURE**" : n.ToString());
+                sum += (BitConverter.ToUInt64(nBytes, 8 * i) % edgeCount) + 1;
             }
-            else
-            {
-                rm.PostReplyLight(msg, n);
-            }
+            
+            rm.PostReplyLight(msg, sum);
         }
     }
 }
